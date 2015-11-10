e7adcc8d75cfb9ff09497548a9701abdaadeaaa2
sheri <sheri.h.zada@gmail.com>
2015-11-10 13:24:34 -0800

    first commit, create table users

---
+++ import_db.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  author_id INTEGER,
+
+  FOREIGN KEY (author) REFERENCES authors(id)
+);
