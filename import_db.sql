a33c99b24651ba61b9968bee5a5b18bffec3a545
sheri <sheri.h.zada@gmail.com>
2015-11-10 15:37:32 -0800

    find authored questions and user by id

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,10 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS question_as_follows;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_likes;
+
+
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   fname VARCHAR(255) NOT NULL,
@@ -10,5 +17,44 @@ CREATE TABLE questions (
   body TEXT NOT NULL,
   author_id INTEGER,
 
-  FOREIGN KEY (author) REFERENCES authors(id)
+  FOREIGN KEY (author_id) REFERENCES users(id)
 );
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER,
+  user_id INTEGER,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER,
+  user_id INTEGER,
+  parent_reply INTEGER,
+  body TEXT NOT NULL,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (parent_reply) REFERENCES replies(id)
+);
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER,
+  user_id INTEGER
+);
+
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Peter', 'Pan'), ('Sam', 'Scott');
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+  ('HELLO', 'WHAT IS THIS?', 1),
+  ('AGE', 'how old are you?', 1),
+  ('room', 'why am i here?', 2);
